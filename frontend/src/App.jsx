3df3a48aa70dadc501d81b19d0514f43a948c94d
Carolaine <karol-21021998@hotmail.com>
2024-10-10 15:46:34 -0300

    fix: removing page profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import { AuthProvider } from "./auth/Context";
 import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile";
 import ToDoList from "./pages/ToDoList";
 import PrivateRoute from "./routes/PrivateRoute";
 import { ToastContainer } from "react-toastify";
@@ -16,7 +15,6 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/todolist" element={<ToDoList />} />
-          <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
       <ToastContainer
